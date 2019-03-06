d32e0da5e784d426bc7cae55998aa3815ab8a3ab
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2019-03-06 11:24:16 +0200

    initial

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="form-container card">
+        <form className="form card-body">
+          <div className="form-group">
+            <label>Username</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter username"
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="form-group">
+            <label>Repeat password</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter repeat password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary w-100">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
