1720c615631ff1c3c4df9b3163d5560554e0eee5
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2019-03-06 11:30:44 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,42 @@
 import React from "react";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      password: "",
+      repeatPassword: ""
+    };
+  }
+
+  onChangeUsername = event => {
+    this.setState({
+      username: event.target.value
+    });
+  };
+
+  onChangePassword = event => {
+    this.setState({
+      password: event.target.value
+    });
+  };
+
+  onChangeRepeatPassword = event => {
+    this.setState({
+      repatPassword: event.target.value
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    // console.log("refs", this.username.value, this.password.value);
+    console.log("submit", this.state);
+  };
+
   render() {
+    // console.log(this);
     return (
       <div className="form-container card">
         <form className="form card-body">
@@ -11,6 +46,10 @@ export default class App extends React.Component {
               type="text"
               className="form-control"
               placeholder="Enter username"
+              ref={node => (this.username = node)}
+              name="username"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
             />
           </div>
           <div className="form-group">
@@ -19,6 +58,10 @@ export default class App extends React.Component {
               type="text"
               className="form-control"
               placeholder="Enter password"
+              ref={node => (this.password = node)}
+              name="password"
+              value={this.state.password}
+              onChange={this.onChangePassword}
             />
           </div>
           <div className="form-group">
@@ -27,9 +70,17 @@ export default class App extends React.Component {
               type="text"
               className="form-control"
               placeholder="Enter repeat password"
+              ref={node => (this.repeatPassword = node)}
+              name="repeatPassword"
+              value={this.state.repeatPassword}
+              onChange={this.onChangeRepeatPassword}
             />
           </div>
-          <button type="submit" className="btn btn-primary w-100">
+          <button
+            type="submit"
+            className="btn btn-primary w-100"
+            onClick={this.onSubmit}
+          >
             Submit
           </button>
         </form>
